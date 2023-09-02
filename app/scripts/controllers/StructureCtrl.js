5fbf24cbd03b29b6aef826443c6e6b047db6c0c8
Nishanthi Dasanayaka <uqndasan@uq.edu.au>
2023-09-03 07:12:45 +1000

    [BUGFIXED]-Updated geocoding api

--- app/scripts/controllers/StructureCtrl.js
+++ app/scripts/controllers/StructureCtrl.js
@@ -135,7 +135,7 @@ angular.module('qldarchApp').controller(
           clearTimeout($scope.typingTimer);
           $scope.typingTimer = setTimeout(function() {
            // $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyByxzrKtppGuOxwzav-P52wTcfXcG1IJz0&address=' + $scope.structure.location,
-	      $.getJSON('http://nominatim.openstreetmap.org/search?q=' + $scope.structure.location+'&format=json',
+	      $.getJSON('https://nominatim.openstreetmap.org/search?q=' + $scope.structure.location+'&format=json',
                 function(data) {
               /*    if (data.results.length === 1) {
                     if ((structure.latitude !== data.results[0].geometry.location.lat) ||
